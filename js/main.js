6cc525982f26870bf0a2a2a6a426968e783dd1a7
bluezebragames <bluezebragames@gmail.com>
2015-05-03 16:54:36 -0600

    Clicking!

--- js/main.js
+++ js/main.js
@@ -1,18 +1,24 @@
 Game = {};
 
 Game.Launch = function() {
-    Game.cps = 1;
+    Game.cps = 0;
     Game.cookies = 0;
+    Game.cookiesd = 0;
     Game.fps = 60;
+}
+
+Game.click = function() {
+    Game.cookies++;
 }
 
 Game.Logic = function() {
     Game.cookies += Game.cps/Game.fps;
+    Game.cookiesd = Math.round(Game.cookies);
 }
 
 Game.Loop = function () {
     var meh = document.getElementById("leftpanel");
-	meh.innerHTML = Game.cookies;
+	meh.innerHTML = Game.cookiesd;
 
     Game.catchuplogic = 0;
     Game.Logic();
@@ -24,4 +30,5 @@ Game.Launch();
 
 window.onload = function() {
     Game.Loop();
+    document.getElementById("leftpanel").addEventListener("click", Game.click, false);
 }
